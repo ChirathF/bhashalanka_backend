853305ec9c6e7d004e1c0b367eb669b2f469ad33
Chirath Fonseka <chirathfonseka98#gmail.com>
2024-08-02 16:03:08 +0530

    setup project

---
+++ src/main/java/com/bhashalanka/test/practical_backend/PracticalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bhashalanka.test.practical_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhashalanka/test/practical_backend/PracticalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhashalanka.test.practical_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
